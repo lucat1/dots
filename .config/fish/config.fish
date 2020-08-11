cd7edc439f8630b2dfdd2b475427c3f6f20ec1f6
Luca <lucat1@users.noreply.github.com>
2020-08-11 21:40:44 +0200

    move custom packages to alpine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,7 @@ set -gx GPG_TTY '$(tty)'
 
 ### modify global $PATH
 set -gx PATH "/sbin" $PATH
+set -gx PATH "/usr/sbin" $PATH
 set -gx PATH "$HOME/.bin" $PATH
 set -gx PATH "$GOPATH/bin" $PATH
 set -gx PATH "$VOLTA_HOME/bin" $PATH
