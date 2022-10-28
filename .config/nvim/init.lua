ca256e19b2ef73097045ac77d97ae2fff549ac6b
Luca <luca.tagliavini5@studio.unibo.it>
2022-10-28 12:10:42 +0200

    update visimp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,6 @@
-require('visimp')({
+require 'visimp' {
   defaults = {
     foldmethod = 'marker',
-  },
-  binds = {
-    [{ mode = 'n', bind = '<C-Tab>' }] = ':bprev',
-    [{ mode = 'n', bind = '<C-S-Tab>' }] = ':bnext',
   },
   lsp = {
     nullls = {
@@ -26,7 +22,6 @@ require('visimp')({
     'go',
     'rust',
   },
-  rust = { lsp = 'rust_analyzer' },
   lspformat = {},
   theme = {
     package = 'ellisonleao/gruvbox.nvim',
@@ -34,16 +29,12 @@ require('visimp')({
     background = 'dark',
     lualine = 'gruvbox',
     before = function()
-<<<<<<< HEAD
-      require('gruvbox').setup({
-=======
       require 'gruvbox'.setup {
->>>>>>> 782a7d2102193fcd770846809211c8f02e03bdf9
         contrast = 'hard',
         overrides = {
           NormalFloat = { link = 'Normal' },
         },
-      })
+      }
     end,
   },
-})
+}
